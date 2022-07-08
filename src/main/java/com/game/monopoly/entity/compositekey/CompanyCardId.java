b02f3a256a6b34e83eeec9bd08bd1772929d36b9
mlyagusheva <green.day.forever@mail.ru>
2022-07-08 14:55:24 +0300

    add: part of session initialization

--- src/main/java/com/game/monopoly/entity/compositekey/CompanyCardId.java
+++ src/main/java/com/game/monopoly/entity/compositekey/CompanyCardId.java
@@ -1,12 +1,16 @@
 package com.game.monopoly.entity.compositekey;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 import java.io.Serializable;
 
 @Embeddable
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class CompanyCardId implements Serializable {
     private Long cardId;
     private String sessionId;

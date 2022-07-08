a9c2de47ed6eca483a0044e7b8b3492591d278fd
ivkopylovv <vanyakopylovv@mail.ru>
2022-07-09 00:02:36 +0300

    add: global cors config, resource config

---
+++ src/main/java/com/game/monopoly/config/CorsWebConfig.java
@@ -0,0 +1,19 @@
+package com.game.monopoly.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsWebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowCredentials(true);
+    }
+}
